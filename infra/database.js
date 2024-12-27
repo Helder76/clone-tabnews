0b5c5987c49c250321c7bf42d4190f8d1df3c5e7
Helder Freitas <freitas.h@gmail.com>
2024-12-27 15:39:20 -0300

    fix: handle `client` as `underfined` in `database.js`

--- infra/database.js
+++ infra/database.js
@@ -7,10 +7,11 @@ async function query(queryObejct) {
     const result = await client.query(queryObejct);
     return result;
   } catch (error) {
+    console.log("\n Erro dentro do catch do database.js:");
     console.error(error);
     throw error;
   } finally {
-    await client.end();
+    await client?.end();
   }
 }
 
