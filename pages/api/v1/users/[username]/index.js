0738547f2bcb699cf6da5c21190bab46be152f78
Helder Freitas <freitas.h@gmail.com>
2025-04-30 14:55:11 -0300

    feat: add support for PATCH on `/api/v1/users/[username]`

--- pages/api/v1/users/[username]/index.js
+++ pages/api/v1/users/[username]/index.js
@@ -5,6 +5,7 @@ import user from "models/user.js";
 const router = createRouter();
 
 router.get(getHandle);
+router.patch(patchHandle);
 
 export default router.handler(controller.errorHandlers);
 
@@ -12,4 +13,12 @@ async function getHandle(request, response) {
   const username = request.query.username;
   const userFound = await user.findOneByUsername(username);
   return response.status(200).json(userFound);
+}
+
+async function patchHandle(request, response) {
+  const username = request.query.username;
+  const userInputValues = request.body;
+
+  const updatedUser = await user.update(username, userInputValues);
+  return response.status(200).json(updatedUser);
 }
