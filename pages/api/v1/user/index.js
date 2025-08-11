7568b30adbc385887720ffe25b262771754e6e07
Helder Freitas <freitas.h@gmail.com>
2025-08-11 09:59:42 -0300

    feat: implement `/api/v1/user` endpoint with session renewal

---
+++ pages/api/v1/user/index.js
@@ -0,0 +1,21 @@
+import { createRouter } from "next-connect";
+import controller from "infra/controller.js";
+import user from "models/user.js";
+import session from "models/session.js";
+
+const router = createRouter();
+
+router.get(getHandle);
+
+export default router.handler(controller.errorHandlers);
+
+async function getHandle(request, response) {
+  const sessionToken = request.cookies.session_id;
+
+  const sessionObeject = await session.findOneValidByToken(sessionToken);
+  const renewedSessionObject = await session.renew(sessionObeject.id);
+  controller.setSessionCookie(renewedSessionObject.token, response);
+
+  const userFound = await user.findOneById(sessionObeject.user_id);
+  return response.status(200).json(userFound);
+}
