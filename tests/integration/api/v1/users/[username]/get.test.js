f1ea7f0443c78ba0e0131ddec7654686dcf01331
Helder Freitas <freitas.h@gmail.com>
2025-10-01 13:09:54 -0300

    feat: add `features` column to `users` table

--- tests/integration/api/v1/users/[username]/get.test.js
+++ tests/integration/api/v1/users/[username]/get.test.js
@@ -27,6 +27,7 @@ describe("GET /api/v1/users/[username]", () => {
         username: "MesmoCase",
         email: createdUser.email,
         password: responseBody.password,
+        features: [],
         created_at: responseBody.created_at,
         updated_at: responseBody.updated_at,
       });
@@ -56,6 +57,7 @@ describe("GET /api/v1/users/[username]", () => {
         username: createdUser.username,
         email: createdUser.email,
         password: responseBody.password,
+        features: [],
         created_at: responseBody.created_at,
         updated_at: responseBody.updated_at,
       });
